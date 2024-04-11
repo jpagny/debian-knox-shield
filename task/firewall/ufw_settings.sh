824bf9918619769a6a0cdae2927e9fc7e2f86e8a
Jerome Pagny <pagny.jerome@gmail.com>
2024-04-11 10:12:08 +0200

    fix problem ufw config

--- task/firewall/ufw_settings.sh
+++ task/firewall/ufw_settings.sh
@@ -103,6 +103,8 @@ run_action_ufw_settings() {
     ufw allow in 53
 
     # Allow SSH port
+    ufw allow in 22/tcp
+    ufw allow out 22/tcp
     ufw allow in $ssh_port/tcp
     ufw allow out $ssh_port/tcp
 
